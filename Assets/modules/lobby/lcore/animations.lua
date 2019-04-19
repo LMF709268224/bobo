c73a83130756d7f2af64a51d2e07fabbb1ade019
linguohua <lghchinaidea@gmail.com>
2019-04-19 14:21:08 +0800

    增加等待动画播放coplay，以及播放完成后可以不隐藏

--- Assets/modules/lobby/lcore/animations.lua
+++ Assets/modules/lobby/lcore/animations.lua
@@ -38,13 +38,18 @@ local function createGameObject(prefabName)
 	return {holder = holder, go = go, wrapper = wrapper, animator = animator, particles = particles}
 end
 
-local function playGameObject(goCached)
+local function playGameObject(goCached, noAutoHide, coYield)
 	-- 重新激活
 	local parentComponent = goCached.parentComponent
 	goCached.wrapper.visible = true
 	goCached.go:SetActive(false)
 	goCached.go:SetActive(true)
 
+	if noAutoHide then
+		return
+	end
+
+	local co = nil
 	-- 启动定时器，去检测动画是否完成
 	parentComponent:StopTimer(goCached.prefabName)
 	parentComponent:StartTimer(
@@ -68,17 +73,28 @@ local function playGameObject(goCached)
 			parentComponent:StopTimer(goCached.prefabName)
 			goCached.wrapper.visible = false
 			goCached.go:SetActive(false)
+
+			-- 如果需要唤醒coroutine
+			if coYield then
+				local r, err = coroutine.resume(co)
+				if not r then
+					logger.error(debug.traceback(co, err))
+				end
+			end
 		end
 	)
+
+	if coYield then
+		co = coroutine.running()
+		coroutine.yield()
+	end
 end
 
-function AnimationMgr.play(prefabName, parentComponent, x, y)
-	logger.debug("AnimationMgr.play prefabName:", prefabName)
-
+local function getGocached(prefabName, parentComponent, x, y)
 	-- 检查是否有可用的game object，如果有则直接使用
 	local goCached = gameObjectsCached[prefabName]
 	if goCached == nil then
-		logger.debug('AnimationMgr.play, goCached nil, create new game Object for:', prefabName)
+		logger.debug('AnimationMgr.getGocached, goCached nil, create new game Object for:', prefabName)
 
 		goCached = createGameObject(prefabName)
 		gameObjectsCached[prefabName] = goCached
@@ -103,7 +119,21 @@ function AnimationMgr.play(prefabName, parentComponent, x, y)
 	goCached.holder.x = x
 	goCached.holder.y = y
 
-	playGameObject(goCached)
+	return goCached
+end
+
+function AnimationMgr.play(prefabName, parentComponent, x, y, noAutoHide)
+	logger.debug("AnimationMgr.play prefabName:", prefabName)
+
+	local goCached = getGocached(prefabName, parentComponent, x, y)
+	playGameObject(goCached, noAutoHide)
+end
+
+function AnimationMgr.coplay(prefabName, parentComponent, x, y)
+	logger.debug("AnimationMgr.coplay prefabName:", prefabName)
+
+	local goCached = getGocached(prefabName, parentComponent, x, y)
+	playGameObject(goCached, false, true)
 end
 
 return AnimationMgr
