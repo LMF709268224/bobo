d37a90d4ddbb9427433627fee4d59b2c923f50e9
riguang <zscboy@gmail.com>
2019-04-26 11:45:47 +0800

    Merge branch 'master' of https://github.com/LMF709268224/bobo

--- Assets/modules/lobby/scripts/chat/chatView.lua
+++ Assets/modules/lobby/scripts/chat/chatView.lua
@@ -4,6 +4,9 @@
 local ChatView = {}
 local logger = require "lobby/lcore/logger"
 local fairy = require "lobby/lcore/fairygui"
+local httpHelper = require "lobby/lcore/httpHelper"
+local urlpathsCfg = require "lobby/lcore/urlpathsCfg"
+local proto = require "lobby/scripts/proto/proto"
 local CS = _ENV.CS
 
 function ChatView.showChatView()
@@ -15,14 +18,19 @@ function ChatView.showChatView()
         local view = _ENV.thisMod:CreateUIObject("lobby_chat", "chat")
 
         ChatView.viewNode = view
-        --TODO:这里需要初始化列表
-        ChatView.msgList = {}
         ChatView:initView()
         ChatView:testLists()
 
         _ENV.thisMod:RegisterCleanup(
             function()
                 view:Dispose()
+            end
+        )
+        _ENV.thisMod:SetMsgListener(
+            "lobby_chat",
+            function(str)
+                logger.debug("SetMsgListener : ", str)
+                -- ChatView:addMsg()
             end
         )
     end
@@ -58,20 +66,28 @@ function ChatView:initView()
     )
     -- list
     self.phraseList = self.viewNode:GetChild("phraseList").asList
+    self.phraseList.itemRenderer = function(index, obj)
+        self:renderPhraseListItem(index, obj)
+    end
+    self.phraseList:SetVirtual()
+
     self.expressionList = self.viewNode:GetChild("expressionList").asList
+
     self.historyList = self.viewNode:GetChild("historyList").asList
     self.historyList.itemRenderer = function(index, obj)
-        self:renderListItem(index, obj)
+        self:renderHistoryListItem(index, obj)
     end
     self.historyList.itemProvider = function(index)
-        return self:getListItemResource(index)
+        return self:getHistoryListItemResource(index)
     end
     self.historyList:SetVirtual()
 
+    local chatText = self.viewNode:GetChild("chatText")
     local sendBtn = self.viewNode:GetChild("sendBtn")
     sendBtn.onClick:Set(
         function()
-            -- ChatView.win:Hide()
+            self:sendMsg(chatText.text)
+            chatText.text = ""
         end
     )
 end
@@ -95,13 +111,9 @@ function ChatView:changeList(type)
 end
 
 function ChatView:testLists()
-    -- self.expressionItem = _ENV.thisMod:CreateUIObject("lobby_chat", "chat_expression_item")
-    -- self.historyMeItem = _ENV.thisMod:CreateUIObject("lobby_chat", "chat_history_me_item")
-    -- self.historyOtherItem = _ENV.thisMod:CreateUIObject("lobby_chat", "chat_history_other_item")
-    -- self.phraseItem = _ENV.thisMod:CreateUIObject("lobby_chat", "chat_phrase_item")
     self:updatePhraseList()
     self:updateExpressionList()
-    -- self:updateHistoryList()
+    self:updateHistoryList()
 end
 
 -- 更新表情列表
@@ -114,8 +126,7 @@ end
 
 -- 更新短语列表
 function ChatView:updatePhraseList()
-    local phraseMap = {
-        [0] = "你这牌打得也太好了吧。。。",
+    self.phraseMap = {
         [1] = "快点啊，都等到我花都谢了。。。",
         [2] = "真怕猪一样的队友。。。",
         [3] = "一走一停真有型，一秒一卡好潇洒。。。",
@@ -124,54 +135,66 @@ function ChatView:updatePhraseList()
         [6] = "风吹鸡蛋壳，牌去人安乐。。。",
         [7] = "搏一搏，单车变摩托。。。",
         [8] = "我就剩一张牌了。。。",
-        [9] = "炸得好。。。"
+        [9] = "炸得好。。。",
+        [10] = "你这牌打得也太好了吧。。。",
+        [11] = "屌爆了啊",
+        [12] = "我就剩两张牌了。。。"
     }
     self.phraseList.onClickItem:Add(
         function(onClickItem)
-            self:addMsg(onClickItem.data.name == "2", onClickItem.data:GetChild("n0").text)
+            self:sendMsg(onClickItem.data:GetChild("n0").text)
         end
     )
-    self.phraseList:RemoveChildrenToPool()
-    for i = 0, #phraseMap do
-        local obj = self.phraseList:AddItemFromPool()
-        obj.name = i
-        local t = obj:GetChild("n0")
-        t.text = phraseMap[i]
-    end
+
+    self.phraseList.numItems = #self.phraseMap
 end
 
 -- 更新历史列表
--- function ChatView:updateHistoryList()
--- for i = 1, 16 do
---     if i % 2 == 0 then
---         -- self.phraseList:AddItemFromPool()
---         -- self.phraseList.itemRenderer = RenderListItem
---         local obj = _ENV.thisMod:CreateUIObject("lobby_chat", "chat_history_me_item")
---         local t = obj:GetChild("text")
---         t.text = "我说是多少打打 ad大神 阿萨德ad撒 啊大声地啊 阿萨德 ：" .. i
---         self.historyList:AddChild(obj)
---     else
---         local obj = _ENV.thisMod:CreateUIObject("lobby_chat", "chat_history_other_item")
---         local t = obj:GetChild("text")
---         t.text = "ni说是ds  啊大声地啊 阿萨德 ：" .. i
---         local n = obj:GetChild("name")
---         n.text = "我是 " .. i
---         self.historyList:AddChild(obj)
---     end
--- end
--- end
+function ChatView:updateHistoryList()
+    --TODO:这里需要初始化列表
+    self.msgList = {}
+end
+
+function ChatView:sendMsg(str)
+    -- TODO: 请求大厅 发送消息
+    -- local co = coroutine.running()
+    -- 请求服务器获取模块更新信息
+    local tk = CS.UnityEngine.PlayerPrefs.GetString("token", "")
+    local url = urlpathsCfg.rootURL .. urlpathsCfg.chat .. "?tk=" .. tk
+    local rapidjson = require("rapidjson")
+    local jsonString = rapidjson.encode({msg = str})
+    local chat = {
+        scope = proto.lobby.ChatScopeType.InRoom,
+        dataType = proto.lobby.ChatDataType.Text,
+        data = jsonString
+    }
+    local body = proto.encodeMessage("lobby.MsgChat", chat)
+    httpHelper.post(
+        self.viewNode,
+        url,
+        body,
+        function(req, resp)
+            if req.State == CS.BestHTTP.HTTPRequestStates.Finished then
+                logger.debug("send msg ok")
+                logger.debug("---------------------: ", resp.Data)
+            else
+                logger.debug("send msg error : ", req.State)
+            end
+        end
+    )
+    -- coroutine.yield()
+    self:addMsg(true, str)
+end
 
 function ChatView:addMsg(isMe, str)
-    -- logger.debug("addMsg : ", isMe)
     local s = #self.msgList + 1
     self.msgList[s] = {fromMe = isMe, msg = str}
     self.historyList.numItems = s
     self.historyList.scrollPane:ScrollBottom()
 end
 
-function ChatView:getListItemResource(index)
+function ChatView:getHistoryListItemResource(index)
     local msg = self.msgList[index + 1]
-    -- logger.debug("msg : ", msg)
     if msg.fromMe then
         return "ui://lobby_chat/chat_history_me_item"
     else
@@ -179,9 +202,16 @@ function ChatView:getListItemResource(index)
     end
 end
 
-function ChatView:renderListItem(index, obj)
+function ChatView:renderHistoryListItem(index, obj)
     local msg = self.msgList[index + 1]
     local t = obj:GetChild("text")
     t.text = msg.msg
 end
+
+function ChatView:renderPhraseListItem(index, obj)
+    local msg = self.phraseMap[index + 1]
+    local t = obj:GetChild("n0")
+    t.text = msg
+end
+
 return ChatView
